132e4da8befbea92c6daaee5eda75bfb8c6c77bb
uo251851 <marcialfco@live.com>
2019-01-27 19:37:05 +0100

    Location obtained from GPS

---
+++ Assets/Scripts/UpdateGPSText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour {
+
+    public Text coordinates;
+    
+	private void Update ()
+    {
+        coordinates.text = "Lat: " + GPS.Instance.latitude
+            + "\nLon: " + GPS.Instance.longitude;
+    }
+}
