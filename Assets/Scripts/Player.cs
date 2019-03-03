8e41fce8c3f83de059aab71c785808bec6bae98d
uo251851 <marcialfco@live.com>
2019-03-03 23:41:57 +0100

    Added script to control videoplayer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Player : MonoBehaviour {
+
+    public Button playpauseBtn, cancelBtn, stopBtn;
+    public VideoPlayer videoplayer;
+
+	void Start() {
+        playpauseBtn.onClick.AddListener(playVideo);
+        cancelBtn.onClick.AddListener(cancelVideo);
+        stopBtn.onClick.AddListener(stopVideo);
+    }
+	
+	void playVideo() {
+        videoplayer.Play();
+        playpauseBtn.onClick.AddListener(pauseVideo);
+    }
+
+    void pauseVideo()
+    {
+        videoplayer.Pause();
+    }
+
+    void cancelVideo()
+    {
+        //TO-DO
+    }
+
+    void stopVideo()
+    {
+        videoplayer.Stop();
+    }
+}
